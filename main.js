756d69c6657bbf641cffd51e4d41944824e0cfbc
Sunbreeze <matthiassunbreeze@gmail.com>
2022-07-27 00:05:04 +0500

    initialized welcome

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const loader = document.querySelector("#preloader");
-const main = document.querySelector("#main");
+const main = document.querySelector(".greetings-span");
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
     loader.style.display = "none";
 
-    main.style.display = "block";
+    main.style.display = "inline-block";
     setTimeout(() => (main.style.opacity = 1), 50);
   }, 3200);
 }
