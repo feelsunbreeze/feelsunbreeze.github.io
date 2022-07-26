520ef975c77602ff265740c9cb5afea737cc7b23
Sunbreeze <matthiassunbreeze@gmail.com>
2022-07-26 15:38:52 +0500

    updated loader!

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function () {
-  loader.style.display = "none";
-});
+const loader = document.querySelector("#preloader");
+const main = document.querySelector("#main");
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = "none";
+
+    main.style.display = "block";
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 3000);
+}
+init();
