848bf7a9662ef43d9d81d212827911817a58434b
Sunbreeze <matthiassunbreeze@gmail.com>
2022-07-28 02:55:12 +0500

    big update
    added a comic style to text and added info about me!

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const loader = document.querySelector("#preloader");
-const main = document.querySelector(".greetings-span");
+const main = document.querySelector("#body-div");
 function init() {
   setTimeout(() => {
     loader.style.opacity = 0;
     loader.style.display = "none";
 
-    main.style.display = "inline-block";
+    main.style.display = "inline";
     setTimeout(() => (main.style.opacity = 1), 50);
   }, 3200);
 }
